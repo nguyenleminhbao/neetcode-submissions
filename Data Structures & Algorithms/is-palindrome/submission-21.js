6a86174831e9b97a1ba4326aa1a1ad95e111e0a4
nguyenleminhbao <73061006+nguyenleminhbao@users.noreply.github.com>
2026-05-25 22:37:47 +0700

    Add: is-palindrome - submission-21

---
+++ Data Structures & Algorithms/is-palindrome/submission-21.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let str = "";
+
+        for (let ele of s.toUpperCase()) {
+            if ((ele >= "A" && ele <= "Z") || (ele >= "0" && ele <= "9")) {
+                str += ele;
+            }
+        }
+
+        let l = 0;
+        let r = str.length - 1;
+
+        while (l <= r) {
+            const left = str[l];
+            const right = str[r];
+
+            if (left === right) {
+                l++;
+                r--;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
