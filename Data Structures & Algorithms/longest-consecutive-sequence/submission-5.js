932c77da434c0fc81108ba3558f6a0772f2688cb
nguyenleminhbao <73061006+nguyenleminhbao@users.noreply.github.com>
2026-05-25 22:00:33 +0700

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+
+        if (nums.length === 0) return 0
+        
+        const newNums = Array.from(new Set(nums)).sort((a,b) => a-b);
+        console.log(newNums);
+
+        let max = 1; 
+        let longest = max;
+
+        for (let i = 1; i < newNums.length ; i++) {
+            if(newNums[i] - newNums[i-1] === 1) {
+                max+=1;
+            }
+            else {
+                console.log('ERROR', newNums[i])
+                longest = Math.max(max, longest);
+                max = 1;
+            }
+
+             longest = Math.max(max, longest);
+        }
+
+        return longest;
+    }
+}
