1a1e10dd22691aa1e4ea56cf7e11c8405c60ac50
nguyenleminhbao <73061006+nguyenleminhbao@users.noreply.github.com>
2026-05-26 22:38:54 +0700

    Add: daily-temperatures - submission-13

---
+++ Data Structures & Algorithms/daily-temperatures/submission-13.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        if (temperatures.length === 0) return [];
+
+        const result = new Array(temperatures.length).fill(0);
+        const stack = [];
+
+        for (let i = 0; i < temperatures.length; i++) {
+            const currentTemp = temperatures[i];
+
+           
+
+            while (stack.length > 0 && currentTemp > stack[stack.length - 1][1]) {
+                const temp = stack.pop();
+                result[temp[0]] =  i - temp[0];
+            }
+
+            stack.push([i, currentTemp]);
+        }
+
+        return result;
+    }
+}
