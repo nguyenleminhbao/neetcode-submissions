1da6cd2fd4eaf3bc6e25d0df6a18849377a1fe34
nguyenleminhbao <73061006+nguyenleminhbao@users.noreply.github.com>
2026-06-08 23:31:53 +0700

    Add: median-of-two-sorted-arrays - submission-14

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-14.js
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number}
+     */
+    findMedianSortedArrays(nums1, nums2) {
+        let l1 = 0;
+        let l2 = 0;
+        let arr = [];
+        const length = nums1.length + nums2.length;
+
+        const medianIndex = Math.trunc(length / 2);
+        const isEven = length % 2 === 0;
+
+        while (l1 < nums1.length && l2 < nums2.length) {
+            if (nums1[l1] < nums2[l2]) {
+                arr.push(nums1[l1]);
+                l1++;
+            } else {
+                arr.push(nums2[l2]);
+                l2++;
+            }
+
+            if (arr.length - 1 === medianIndex) {
+                if (!isEven) return arr[medianIndex];
+                else {
+                    return (arr[medianIndex] + arr[medianIndex - 1]) / 2;
+                }
+            }
+        }
+
+        if (l1 < nums1.length) {
+            arr = arr.concat(nums1.splice(l1));
+        } else {
+            arr = arr.concat(nums2.splice(l2));
+        }
+
+        if (!isEven) return arr[medianIndex];
+        else {
+            return (arr[medianIndex] + arr[medianIndex - 1]) / 2;
+        }
+
+        return 0;
+    }
+}
