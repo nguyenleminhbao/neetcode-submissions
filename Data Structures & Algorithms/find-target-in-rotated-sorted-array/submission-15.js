192ae850ba372abe5c49d727664358853757c237
nguyenleminhbao <73061006+nguyenleminhbao@users.noreply.github.com>
2026-06-08 23:06:21 +0700

    Add: find-target-in-rotated-sorted-array - submission-15

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-15.js
@@ -0,0 +1,59 @@
+class Solution {
+    findMin(nums) {
+        let left = 0;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const mid = Math.trunc((left + right) / 2);
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    binarySearch(nums, target) {
+        let left = 0;
+        let right = nums.length - 1;
+
+        while (left <= right) {
+            const mid = Math.trunc((left + right) / 2);
+
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        const indexMin = this.findMin(nums);
+        const sorted1 = [...nums].splice(0, indexMin);
+        const sorted2 = [...nums].splice(indexMin);
+
+        const index1 = this.binarySearch(sorted1, target);
+
+      
+
+        if (index1 !== -1) {
+            return index1;
+        } else {
+            const index2 = this.binarySearch(sorted2, target);
+            return index2 !== -1 ? indexMin + index2 : index2;
+        }
+    }
+}
